0e2fb3f9520402a340a20d5aee00dee96c97a3a1
Jose <joseastu3345@gmail.com>
2026-04-21 19:59:15 -0500

    MBurbujaAvanzado

--- src/App.java
+++ src/App.java
@@ -1,6 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
         metodoBurbuja();
+        runMetodoBurbujaAvanzado();
+    }
+    public static void runMetodoBurbujaAvanzado() {
+        // 1ro instanciar la clase
+        int[] arreglo = new int[]{50, 5, 20, 30, 0, -10, 15};
+        //                       {5, (50), 20, 30, 0, -10, 15}
+        //                       {5, 20, (50), 30, 0, -10, 15}
+        //                       {5, 20, 30, (50), 0, -10, 15} 1. Tamaño del arreglo -1, 2. Quitar indice de completados
+        //                       {5, 20, 30, 0, (50), -10, 15}
+        MetodoBurbujaAvanzado mBurbujaAvanzado = new MetodoBurbujaAvanzado(arreglo);
+        mBurbujaAvanzado.printArreglo(); //imprimir el arreglo NORMAL
+        mBurbujaAvanzado.sort(true);// Ordenar el arreglo ascendente
+        mBurbujaAvanzado.printArreglo(); //Imprimir el arreglo ASCENDENTE
+        mBurbujaAvanzado.sort(false); // Ordenar el arreglo descendente
+        mBurbujaAvanzado.printArreglo(); // Imprimir el arreglo descendente
     }
     public static void metodoBurbuja(){
         System.out.println("Método Burbuja");
@@ -11,5 +26,6 @@ public class App {
         metodoBurbuja.imprimirArreglo();
         metodoBurbuja.ordenarDescendente();
         metodoBurbuja.imprimirArreglo();
+
     }
 }
---
+++ src/MetodoBurbujaAvanzado.java
@@ -0,0 +1,52 @@
+public class MetodoBurbujaAvanzado {
+    int arreglo[];
+    public MetodoBurbujaAvanzado(int[] arreglo){
+        this.arreglo = arreglo;
+    }
+    public void sort(boolean asc){ //<>
+        int tam = arreglo.length;
+        int contComparaciones = 0; //contador para cada cambio
+        int contCambios = 0; //contador para cada interaccion de comparacion
+        boolean huboIntercambios;
+        for (int i = 0; i < tam-1; i++){ //-1 por el rango 
+            huboIntercambios = false; //se bloquea cada que no hay intercambios, false: break 
+            for(int j = 0; j < tam -1 -i; j++){
+                contComparaciones++;
+                if (arreglo[j] > arreglo[j+1]){
+                    int aux = arreglo[j];
+                    arreglo[j] = arreglo[j+1];
+                    arreglo[j+1] = aux;} 
+                    //sort es para odenar ascendentemente o descendentemente
+                if (asc){
+                    //true -- ascendente
+                    if (arreglo[j] > arreglo[j+1]){
+                    int aux = arreglo[j];
+                    arreglo[j] = arreglo[j+1];
+                    arreglo[j+1] = aux;}
+                    huboIntercambios = true;
+                }
+                else{
+                    //false descendente
+                    if (arreglo[j] < arreglo[j+1]){
+                        contCambios++;
+                        int aux = arreglo[j];
+                        arreglo[j] = arreglo[j+1];
+                        arreglo[j+1] = aux; }
+                    
+                }
+                if (!huboIntercambios){ //no hay intercambios significa que todo está ordenado, si todo está ordenado detengo el
+                    //bucle, porque de lo contrario voy a estar ordenando cosas que ya ordené
+                    break;
+                }
+                }
+                }
+        System.out.println("Comparaciones: " + contComparaciones);
+        System.out.println("Cambios: " + contCambios);
+    }
+    public void printArreglo(){
+        for (int valor: arreglo){
+            System.out.print(valor + ", ");
+        }
+        System.out.println();
+    }
+    }
